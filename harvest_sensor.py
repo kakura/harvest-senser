8cd479e8506904c0fccd9690b939a8a9334d4e02
ykarakita <taf.749@gmail.com>
2020-09-19 19:32:47 +0900

    feature: upload sensor data

--- harvest_sensor.py
+++ harvest_sensor.py
@@ -2,8 +2,29 @@ from sensors.air_sensor import AirSensor
 from sensors.ground_sensor import GroundSensor
 
 import time
+import socket
+import json
 
 INTERVAL = 0.1 # minutes
+SORACOM_ENDPOINT = 'funnel.soracom.io'
+SORACOM_PORT     = 23080
+
+
+def camelize(str):
+    return str[0].lower() + str[1:]
+
+
+class SoracomClient:
+    def upload_data(self, data):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((SORACOM_ENDPOINT, SORACOM_PORT))
+            s.sendall(self.build_upload_data(data))
+            data = s.recv(1024)
+
+    def build_upload_data(self, data):
+        res_data =  json.dumps(data).encode('utf-8')
+        return res_data
+
 
 class HarvestSensor:
     def __init__(self):
@@ -14,17 +35,23 @@ class HarvestSensor:
 
     def measure(self):
         self.__setup_sensors()
-        self.__measure()
+        sensor_data = self.__measure()
         self.__cleanup_sensors()
+
+        return sensor_data
     
     def __setup_sensors(self):
         for sensor in self.sensors:
             sensor.setup()
 
     def __measure(self):
+        sensor_data = {}
+
         for sensor in self.sensors:
             result = sensor.measure()
-            print(result)
+            sensor_data[camelize(sensor.__class__.__name__)] = result
+        
+        return sensor_data
 
     def __cleanup_sensors(self):
         for sensor in self.sensors:
@@ -33,7 +60,9 @@ class HarvestSensor:
 
 if __name__ == "__main__":
     harvest_sensor = HarvestSensor()
+    soracom = SoracomClient()
 
     while True:
-        harvest_sensor.measure()
+        result = harvest_sensor.measure()
+        soracom.upload_data(result)
         time.sleep(INTERVAL * 60)
